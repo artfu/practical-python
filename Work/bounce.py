f13ded400f6b6a6de2a384438a15cddb0292220a
artfu <fuxin92@gmail.com>
2021-05-11 15:32:51 +0800

    update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce_time = 0
+while bounce_time < 10:
+    height = round(height * 3/5, 4)
+    bounce_time = bounce_time + 1
+    print(bounce_time, height)
