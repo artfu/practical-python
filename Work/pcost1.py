9e7379efc60622cca89ca2f1e04f82b1a6deca4e
artfu <fuxin92@gmail.com>
2021-05-12 23:15:16 +0800

    update pcost

--- Work/pcost1.py
+++ Work/pcost1.py
@@ -1,15 +1,18 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.31
 def portfolio_cost(filename):
     total_price = 0
     f = open(filename, 'rt')
     headers = next(f).split(',')
     for line in f:
         row = line.split(',')
-        total_price = total_price + (int(row[1]) * float(row[2]))
+        try:
+            total_price = total_price + (int(row[1]) * float(row[2]))
+        except ValueError:
+            print(f'{row} has BAD INPUT')
     f.close()
     return total_price
 
-cost = portfolio_cost('Data/portfolio.csv')
+cost = portfolio_cost('Data/missing.csv')
 print(f'Total cost: {cost}')
--- Work/pcost2.py
+++ Work/pcost2.py
@@ -1,15 +1,23 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.32
+import csv
+import sys
+
 def portfolio_cost(filename):
     total_price = 0
     f = open(filename, 'rt')
-    headers = next(f).split(',')
-    for line in f:
-        row = line.split(',')
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
         total_price = total_price + (int(row[1]) * float(row[2]))
     f.close()
     return total_price
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
 
 cost = portfolio_cost('Data/portfolio.csv')
 print(f'Total cost: {cost}')
